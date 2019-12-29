2dbcc7f3fa5684d60d956e931b7b5875e9aa8f5b
Alexis M <iamsnkh@pm.me>
2019-12-29 07:44:18 +0100

    :sparkles: finished

--- srcs/entrypoint.sh
+++ srcs/entrypoint.sh
@@ -2,12 +2,15 @@
 chmod +x mkcert
 ./mkcert localhost
 
-cp configs/nginx_autoindex.conf /etc/nginx/sites-enabled/
+cp configs/nginx.conf  /etc/nginx/sites-enabled/
+
+# Starting and setup mysql
+service mysql start
+source database/init_db.sh
 
 # Starting services! 
 /etc/init.d/php7.3-fpm start
 service nginx start
-service mysql start
 
 # Infinite loop.. 
 tail -f /dev/null
