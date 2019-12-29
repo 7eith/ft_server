f82ed99e5d07e495c423957e4e7af526135cfa82
Alexis M <iamsnkh@pm.me>
2019-12-29 05:57:51 +0100

    :sparkles: add  php-fpm and certificate

--- srcs/entrypoint.sh
+++ srcs/entrypoint.sh
@@ -1,3 +1,14 @@
+
+
+# MkCert
+chmod +x mkcert
+./mkcert localhost
+
+rm /etc/nginx/sites-enabled/defaut
+cp configs/nginx_autoindex.conf /etc/nginx/sites-enabled/
+
+/etc/init.d/php-fpm start
+
 service nginx start
 service mysql start
 tail -f /dev/null
