5f1694535caf79e6a01f6e5298bab73e33f91c99
Alexis Montelimart <amonteli@z4r3p4.le-101.fr>
2020-01-08 17:03:12 +0100

    :sparkles: ref for use localhost instead amonte.li and compress binaries

--- srcs/entrypoint.sh
+++ srcs/entrypoint.sh
@@ -1,16 +1,20 @@
+# Extract web srcs
+tar xvzf web.tar.gz
+
 # Generate auto SSL certificate with MkCert
 chmod +x mkcert
 ./mkcert localhost
 
-cp configs/nginx.conf  /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-enabled/default
+cp configs/nginx.conf /etc/nginx/sites-enabled/
 
 # Starting and setup mysql
 service mysql start
 source database/init_db.sh
 
-# Starting services! 
+# Starting services!
 /etc/init.d/php7.3-fpm start
 service nginx start
 
-# Infinite loop.. 
-tail -f /dev/null
+# Infinite loop..
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
